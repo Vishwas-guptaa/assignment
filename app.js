1a70aa7683d4443fa9c68a2dac85c29ad693c789
Vishwas-guptaa <vishwasgupta81779@gmail.com>
2023-10-09 15:21:51 +0530

    issue fix

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors =require('cors')
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -10,7 +11,7 @@ const connectdb = require('./db/connectDb')
 
 dotenv.config({ path: ".env" })
 connectdb();
-
+app.use(cors());
 app.use(express.json())
 
 //route load
